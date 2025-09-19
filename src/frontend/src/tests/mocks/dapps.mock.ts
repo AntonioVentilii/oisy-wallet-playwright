6a53d11db9783895286a64c702bbc2c76d3e7b16
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-19 14:00:57 +0200

    clean

--- src/frontend/src/tests/mocks/dapps.mock.ts
+++
@@ -1,4 +0,0 @@
-import { dAppDescriptions } from '$env/dapp-descriptions.env';
-import type { OisyDappDescription } from '$lib/types/dapp-description';
-
-export const mockDappsDescriptions: OisyDappDescription[] = dAppDescriptions;
